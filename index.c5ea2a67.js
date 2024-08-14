c54800d375c400a89ea9efd31e18c482efe87d22
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-08-14 22:10:30 +0300

    make build

---
+++ index.c5ea2a67.js
@@ -0,0 +1,2 @@
+!function(t){for(var e=document.querySelector("table"),r=0;r<e.rows.length;r++)e.rows[r].insertCell(4).textContent=t[r]}(function(t){for(var e=document.querySelector("table"),r=[],n=0;n<e.rows.length;n++){var o=e.rows[n].cells[1];o&&r.push(o.textContent.trim())}return r}(0));
+//# sourceMappingURL=index.c5ea2a67.js.map
